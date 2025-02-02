ae0bddb79b29c4fd303e79ff03802693cf63588d
Jericho de Guzman <poloman7190@gmail.com>
2025-02-02 08:36:36 -0800

    Co-authored-by: Jeremiah de Guzman <jerrymiahdg@users.noreply.github.com> Co-authored-by: Lakshya Mehta <l6kshy6@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,32 +6,109 @@ import title from './assets/Title.png'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [data, setData] = useState("nothing")
-  useEffect(() => {
-    fetch("http://localhost:3000/data").then((data) => data.json().then(idk => setData(idk)))
-  }, [data])
+  const [data, setData] = useState(0)
+  const [iH, setIH] = useState(0)
+  const [iName, setIName] = useState("");
+  const [height, setHeight] = useState(false)
+  const [containers, setContainers] = useState([])
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     fetch("http://localhost:3000/data").then((data) => data.json().then(idk => changeFullness(idk)))
+  //   }, 1000)
+
+  // }, [data])
+
+  // useEffect(() => {
+  //   console.log("banana")
+  //   if (!init.current) {
+  //     init.current = true;
+  //     return;
+  //   }
+  //   setContainers((containers) => {
+  //     console.log(containers)
+  //     if (containers) containers.unshift({data: 0, height: height})
+  //     return containers;
+  //   })
+  // }, [height])
+
+  let percent = 100 - Math.round(Number(data) / height * 100)
+  if (percent < 0) percent = 0;
+
+  const getPercent = (data, height) => {
+    let percent = 100 - Math.round(Number(data) / height * 100)
+    if (percent < 0) percent = 0;
+    return percent;
+  }
+
+  const changeFullness = (value) => {
+    setData(value)
+    setContainers((containers) => {
+      if (containers.length == 0) return [];
+      const copy = [...containers]
+      copy[0].data = value;
+      copy[0].percent = getPercent(value, copy[0].height)
+      return copy;
+    })
+  }
+
+  const addContainer = () => {
+    if (iH <= 0 || !iName) return;
+    setContainers((containers) => {
+      const copy = [...containers]
+      console.log(containers)
+      if (copy) copy.unshift({ data: 0, height: iH, name: iName, key: Math.random() * 10000 })
+      console.log("updating")
+      console.log(containers)
+      setHeight(!height)
+      return copy;
+    })
+    setIH(0)
+    setIName("")
+  }
 
   return (
     <div>
+      <input onChange={(e) => changeFullness(e.target.value)} />
       <img src={title} width={300} />
+
       <header>Your Containers</header>
+      <div className='inputContainer'>
+        <div className='midinput'>
+          <label className='iCChild'>Name</label>
+          <input className='iCChild' value={iName} onChange={(e) => setIName(e.target.value)} />
+        </div>
+        <div className='midinput'>
+          <label className='iCChild'>Height (cm)</label>
+          <input className='iCChild' value={iH} onChange={(e) => setIH(e.target.value)} />
+        </div>
+        <button className='iCChild' type='button' onClick={() => setIH(data)}>Calibrate</button>
+        <button className='iCChild' type='button' onClick={addContainer}>Add Container</button>
+      </div>
+      {/* {containers.map(item => (<div>
+        <p>{item.data}</p>
+        <p>{item.height}</p>
+      </div>))} */}
       <div className='tile-grid'>
-        {[0,1,2,3].map(item => (<div className='tile'>
-          <p>Trash Bin 1</p>
-          <div class="info">
-            <div class="icon">
-              <div class="side"></div>
-              <div id="filling"></div>
-              <div class="side"></div>
-            </div>
-            <div id="text-info">
-              <div class="bar"></div>
-              <div class="text-text">
-                <p id="percentage">50% full</p>
-                <p>6 ft</p>
+        {containers.map(item => (<div key={item.key}>
+          {/* <div className='tile-out'> */}
+          <div className='tile'>
+            <p className='banana'>{item.name}</p>
+            <div class="info">
+              <div class="icon">
+                <div class="side"></div>
+                <div style={{ height: `${item.percent}%` }} id="filling"></div>
+                <div class="side"></div>
+              </div>
+              <div id="text-info" style={{ height: `${item.percent > 0 ? item.percent : 100}%` }}>
+                <div class="bar" style={{ opacity: item.percent > 0 ? '100' : '0' }}></div>
+                <div class="text-text">
+                  <div id="percentage">{item.percent}% full</div>
+                  <div>{(item.height - Math.round(item.data)) > 0 ? item.height - Math.round(item.data) : 0} cm of {item.height} cm</div>
+                </div>
               </div>
             </div>
           </div>
+          {/* </div> */}
         </div>))}
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors")
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.text())
 
 app.use(cors({
   origin: "http://localhost:5173", credentials: true
@@ -13,13 +18,9 @@ app.get("/data", (req, res) => {
   res.json(data);
 });
 
-app.get("/data", (req, res) => {
-  res.json({ name: "name" });
-});
-
 app.post("/update", (req, res) => {
-  console.log(res.text);
-  data = res.text;
+  console.log(req.body);
+  data = req.body;
 });
 
 app.listen(port, () => {
